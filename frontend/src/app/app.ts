d376a63153c9b8e2525d57ebe2d28a787dadd080
Emmanuel Hesry <ehesry@gmail.com>
2026-01-09 09:54:19 +0100

    Add Angular Material (M3 theming) + initial UI components (slide-toggle, button)

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
