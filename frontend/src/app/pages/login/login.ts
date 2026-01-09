872cb9906b7f67bfa2c4d54a1b423da6f6779bd6
Emmanuel Hesry <ehesry@gmail.com>
2026-01-09 12:46:55 +0100

    Add Auth UI
    - Add Auth service to call backend API
    - Add Login component and UI
    - Store token in browser local storage

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, MatInputModule, MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  username = 'demo';
+  password = 'demo';
+  error: string | null = null;
+  loading = false;
 
+  constructor(private authService: Auth) {}
+
+  submit() {
+    this.error = null;
+    this.loading = true;
+
+    this.authService.login(this.username, this.password).subscribe({
+      next: () => {
+        this.loading = false;
+        console.log('Login OK, token stored');
+      },
+      error: () => {
+        this.loading = false;
+        this.error = 'Invalid credentials';
+      },
+    });
+  }
 }
---
+++ frontend/src/app/services/auth.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+
+interface LoginResponse {
+  tokenType: string;
+  accessToken: string;
+  expiresInSeconds: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class Auth{
+  private readonly TOKEN_KEY = 'auth_token';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string) {
+    return this.http
+      .post<LoginResponse>('/api/v1/auth/login', {
+        username,
+        password,
+      })
+      .pipe(
+        tap((response) => {
+          localStorage.setItem(this.TOKEN_KEY, response.accessToken);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+}
