fd019f8ad54e54dcf84988edb34d78c1ca54cad9
Emmanuel Hesry <ehesry@gmail.com>
2026-01-23 14:27:21 +0100

    Refactor Task Dialog to Reactive Forms (create & edit)

--- frontend/src/app/pages/project-detail/task-dialog.ts
+++ frontend/src/app/pages/project-detail/task-dialog.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { Component, inject, OnInit } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle } from "@angular/material/dialog";
@@ -9,7 +9,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
 import { CreateTaskRequest, Task, TaskStatus } from "../../services/tasks";
 import { TaskDialogData, TaskDialogResult } from './task-dialog.types';
-
+import { toIsoLocalDateString } from "../../shared/functions/date";
 @Component({
   selector: 'task-dialog',
   templateUrl: 'task-dialog.html',
@@ -18,7 +18,7 @@ import { TaskDialogData, TaskDialogResult } from './task-dialog.types';
   imports: [
     MatButtonModule,
     MatIconModule,
-    FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatDialogTitle,
@@ -28,14 +28,19 @@ import { TaskDialogData, TaskDialogResult } from './task-dialog.types';
     MatSelectModule,
   ],
 })
-export class TaskDialog {
+export class TaskDialog implements OnInit {
   readonly dialogRef = inject(MatDialogRef<TaskDialog>);
   readonly data = inject<TaskDialogData>(MAT_DIALOG_DATA);
+  readonly formBuilder = inject(FormBuilder);
 
-  title = '';
-  description = '';
-  dueDate: string = new Date().toISOString().split('T')[0];
-  status: TaskStatus = 'TODO';
+  submitAttempted = false;
+
+  form = this.formBuilder.group({
+    title: this.formBuilder.control('', {validators: [Validators.required], nonNullable: true }),
+    description: this.formBuilder.control('', { nonNullable: true }),
+    dueDate: this.formBuilder.control<Date>(new Date(), { nonNullable: true }),
+    status: this.formBuilder.control<TaskStatus>('TODO', {validators: [Validators.required], nonNullable: true }),
+  });
 
   get isEdit(): boolean {
     return this.data.mode === 'edit';
@@ -49,10 +54,6 @@ export class TaskDialog {
     return this.isEdit ? 'Save' : 'Create';
   }
 
-  get isValid(): boolean {
-    return this.title.trim().length > 0;
-  }
-
   ngOnInit() {
     if (this.isEdit && this.data.task) {
       this.prefillFromTask(this.data.task);
@@ -60,13 +61,19 @@ export class TaskDialog {
   }
 
   clickOnOk(): void {
-    if (!this.title.trim()) return;
+    this.submitAttempted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    const formValues = this.form.getRawValue();
 
     const payload: CreateTaskRequest = {
-      title: this.title.trim(),
-      description: this.description?.trim() || undefined,
-      dueDate: this.dueDate || undefined,
-      status: this.status,
+      title: formValues.title.trim(),
+      description: formValues.description.trim(),
+      dueDate: toIsoLocalDateString(formValues.dueDate),
+      status: formValues.status,
     };
 
     const result: TaskDialogResult = this.isEdit
@@ -81,9 +88,12 @@ export class TaskDialog {
   }
 
   private prefillFromTask(task: Task) {
-    this.title = task.title ?? '';
-    this.description = task.description ?? '';
-    this.status = task.status ?? 'TODO';
-    this.dueDate = task.dueDate ?? this.dueDate;
+    this.form.patchValue({
+      title: task.title,
+      description: task.description ?? '',
+      status: task.status,
+      dueDate: task.dueDate ?? new Date(),
+    });
   }
 }
+
