37b850767d7d4e41e5d0bf3308a198e8c5d069d1
Emmanuel Hesry <ehesry@gmail.com>
2026-04-04 12:59:21 +0200

    Fix SonarQube issue in matomo js

--- backend/src/main/resources/static/js/matomo-swagger.js
+++ backend/src/main/resources/static/js/matomo-swagger.js
@@ -1,17 +1,24 @@
 (function () {
-  var _paq = (window._paq = window._paq || []);
-  _paq.push(['disableCookies']);
-  _paq.push(['setDoNotTrack', true]);
-  _paq.push(['HeatmapSessionRecording::disable']);
-  _paq.push(['enableLinkTracking']);
-  _paq.push(['trackPageView']);
+  const _paq = (globalThis._paq = globalThis._paq || []);
+  _paq.push(
+    ['disableCookies'],
+    ['setDoNotTrack', true],
+    ['HeatmapSessionRecording::disable'],
+    ['enableLinkTracking'],
+    ['trackPageView']);
 
   (function () {
-    var u = 'https://stats.manooweb.fr/';
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '3']);
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.async = true; g.defer = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+    const u = 'https://stats.manooweb.fr/';
+    _paq.push(
+      ['setTrackerUrl', u + 'matomo.php'],
+      ['setSiteId', '3']);
+    const d = document;
+    const g = d.createElement('script');
+    const s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.defer = true;
+    g.src = u + 'matomo.js';
+    s.parentNode.insertBefore(g, s);
   })();
 })();
 
