fe5c373af21ccfab55f9dcb03bf46dc28bc08737
Emmanuel Hesry <ehesry@gmail.com>
2026-04-23 19:32:42 +0200

    Format js

--- frontend/src/environments/environment.azure.ts
+++ frontend/src/environments/environment.azure.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://springboot-rest-api-demo-backend.mangocliff-83abab0c.francecentral.azurecontainerapps.io',
+  apiBaseUrl:
+    'https://springboot-rest-api-demo-backend.mangocliff-83abab0c.francecentral.azurecontainerapps.io',
 };
